df46e806c7e0df8de9c6cbdbdf6d1f27bcbad6c9
Yannick Alexander <yannick@alexanderdev.io>
2022-08-14 13:28:55 +0200

    added initial dataflow code

--- dataflow/jobs.go
+++ dataflow/jobs.go
@@ -2,27 +2,96 @@ package dataflow
 
 import (
 	"context"
+	"errors"
+	"time"
+
+	dataflow "google.golang.org/api/dataflow/v1b3"
 )
 
-type JobsService interface {
-	List(projectId string, location string) JobsRequest
+// Types
+
+type Job struct {
+	Id        string
+	Name      string
+	Status    Status
+	StartTime time.Time
 }
 
-type JobsRequest interface {
-	Pages(context.Context, func(JobsResponse) error) error
+type Status struct {
+	status     string
+	updateTime time.Time
 }
 
-type JobsResponse interface{}
+func (s Status) IsNewer(t time.Time) bool {
+	return t.Before(s.updateTime)
+}
 
-func ListJobs(service JobsService, projectId string, location string) {
+func (s Status) IsFailed() bool {
+	return s.status == "JOB_STATE_FAILED"
+}
+
+func (s Status) IsCanceled() bool {
+	return s.status == "JOB_STATE_CANCELLED"
+}
+
+func (s Status) IsDone() bool {
+	return s.status == "JOB_STATE_DONE"
+}
+
+func (s Status) IsRunning() bool {
+	return s.status == "JOB_STATE_RUNNING"
+}
+
+// Functions
+
+func ListJobs(projectId string, location string) ([]Job, error) {
 	ctx := context.Background()
-	req := service.List(projectId, location)
 
-	err := req.Pages(ctx, func(res JobsResponse) error {
+	// Configure Service
+	dataflowService, err := dataflow.NewService(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	jobsService := dataflow.NewProjectsLocationsJobsService(dataflowService)
+	listRequest := jobsService.List(projectId, location)
+
+	// Request Pages
+	var jobs []Job
+	err = listRequest.Pages(ctx, func(res *dataflow.ListJobsResponse) error {
+		for _, job := range res.Jobs {
+			// Parse Timestamps
+			updateTime, err := time.Parse(time.RFC3339, job.CurrentStateTime)
+			if err != nil {
+				return errors.New("couldn't parse time")
+			}
+
+			startTime, err := time.Parse(time.RFC3339, job.StartTime)
+			if err != nil {
+				return errors.New("couldn't parse time")
+			}
+
+			// Parse Job
+			j := Job{
+				Id:   job.Id,
+				Name: job.Name,
+				Status: Status{
+					status:     job.CurrentState,
+					updateTime: updateTime,
+				},
+				StartTime: startTime,
+			}
+
+			jobs = append(jobs, j)
+		}
+
 		return nil
 	})
 
 	if err != nil {
-		println("Failed!")
+		return nil, err
 	}
+
+	// Return Jobs
+	return jobs, nil
 }
--- main.go
+++ main.go
@@ -1,1 +1,47 @@
-package dataflowmonitor
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-co-op/gocron"
+	"github.com/yannickalex07/dataflow-monitor/dataflow"
+)
+
+func main() {
+	projectId := "trv-master-data-pipeline-prod"
+	location := "europe-west4"
+	// timeout := 3 * time.Hour
+
+	lastRun := time.Now()
+	f := func() {
+		// Get All Jobs
+		jobs, err := dataflow.ListJobs(projectId, location)
+		if err != nil {
+			return
+		}
+
+		// Check Newer Jobs
+		for _, job := range jobs {
+			// Check Status Updates
+			if job.Status.IsNewer(lastRun) {
+				println("Job " + job.Name + " has newer status")
+			}
+
+			// Check Run Length
+			runLength := time.Since(job.StartTime)
+
+			fmt.Printf("Job %s is running for %s\n", job.Name, runLength)
+			// if runLength > timeout {
+			// 	println("Job " + job.Name + " ran for too long")
+			// }
+		}
+
+		// update time
+		lastRun = time.Now()
+	}
+
+	s := gocron.NewScheduler(time.UTC)
+	s.Every(30).Seconds().Do(f)
+	s.StartBlocking()
+}
